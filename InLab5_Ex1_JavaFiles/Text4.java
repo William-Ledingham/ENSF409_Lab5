276bf2468b024b41bbcd23e9911c9e7c3bcba598
William Ledingham <williamledingham1@gmail.com>
2020-02-28 18:58:44 -0700

    In Lab completed and handed in

---
+++ InLab5_Ex1_JavaFiles/Text4.java
@@ -0,0 +1,50 @@
+
+class Text implements Cloneable
+{
+	
+	private final Double DEFAULT_SIZE = 10.0;
+	
+    private Colour colour;
+    private Double fontSize;
+    
+    private String text;
+
+
+	public Text(String text) {
+       this.text = text;
+       fontSize = DEFAULT_SIZE;
+	}
+	
+	public Double getFontSize(){
+		return fontSize;
+	}
+	
+	public void setColour(String s){
+		colour = new Colour(s);
+	}
+	
+	public void setText(String newText){
+		text = newText;
+	}
+	
+	public String getText(){
+		return text ;
+	}
+	
+	public Object clone() throws CloneNotSupportedException
+	{
+		Text obj = (Text)super.clone();
+		if(colour != null)
+			obj.colour = (Colour)colour.clone();
+		else
+			obj.colour = null;
+		return obj;
+	}
+	
+	@Override
+	public String toString(){
+		return (text);
+	}
+
+       
+}
